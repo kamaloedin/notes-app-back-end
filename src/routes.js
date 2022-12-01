31738aa659a4dfc903347692d33fe86cb0f3b777
mkama <kamalimaduddin@gmail.com>
2022-12-01 09:43:25 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line object-curly-newline
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handlers');
+
+const routes = [
+  {
+    path: '/notes',
+    method: 'POST',
+    handler: addNoteHandler,
+  },
+  {
+    path: '/notes',
+    method: 'GET',
+    handler: getAllNotesHandler,
+  },
+  {
+    path: '/notes/{id}',
+    method: 'GET',
+    handler: getNoteByIdHandler,
+  },
+  {
+    path: '/notes/{id}',
+    method: 'PUT',
+    handler: editNoteByIdHandler,
+  },
+  {
+    path: '/notes/{id}',
+    method: 'DELETE',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
