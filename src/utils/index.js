3a069399d639fb6f2d33e945437b2b0a0520b289
mkama <kamalimaduddin@gmail.com>
2023-06-21 14:00:18 +0700

    collaboration implemented

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable camelcase */
 // eslint-disable-next-line object-curly-newline
-const mapDBToModel = ({ id, title, tags, body, created_at, updated_at }) => ({
+const mapDBToModel = ({ id, title, tags, body, created_at, updated_at, username }) => ({
   id,
   title,
   tags,
   body,
   createdAt: created_at,
   updatedAt: updated_at,
+  username,
 });
 
 module.exports = { mapDBToModel };
